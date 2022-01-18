0e384f981c7d67927325b6a41e1b9e109030764f
br <92319135+rrraaa890@users.noreply.github.com>
2022-01-18 23:01:43 +0900

    Update admin.php

--- admin.php
+++ admin.php
@@ -5,8 +5,8 @@ define( 'PASSWORD', 'adminPassword');
 
 // データベースの接続情報
 define( 'DB_HOST', 'localhost');
-define( 'DB_USER', 'ryub');
-define( 'DB_PASS', 'ryuji890');
+define( 'DB_USER', 'root');
+define( 'DB_PASS', 'root');
 define( 'DB_NAME', 'board');
 
 // タイムゾーン設定
